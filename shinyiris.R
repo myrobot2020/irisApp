f4c629e10ad350504bf5d903234c5173d0cb90fe
Nik <33054349+myrobot2020@users.noreply.github.com>
2025-01-07 19:04:36 +0530

    Update shinyiris.R

--- shinyiris.R
+++ shinyiris.R
@@ -2,37 +2,24 @@ library(shiny)
 library(waiter)
 
 ui <- fluidPage(
-  textInput("t","t"),
+  plotOutput("b"),
+  actionButton("c","c"),
   selectInput("a","a",choices = unique(iris$Species),selected = "setosa"),
-  plotOutput("b",click = "plot_click"),
   use_waiter()
 )
 
 server <- function(input, output, session) {
   w<-Waiter$new(id = "b")
-  #thematic::thematic_shiny()
-  #timer <- reactiveTimer(50000)
   r<-reactive({
-    #timer()
     iris[iris$Species==input$a,"Sepal.Length"]
   })
   
-  datas <- reactive({
-    req(input$t)
-    
-    exists <- exists(input$t, "setosa")
-    shinyFeedback::feedbackDanger("t", !exists, "Unknown spec")
-    req(exists, cancelOutput = TRUE)
-    
-    get(input$t, "setosa")
-  })
-  
-  
-  
-  output$b<-renderPlot({
+  observeEvent(input$c,{
+    output$b<-renderPlot({
     w$show()
     Sys.sleep(2)
     plot(r())
+  })
   })
 }
 
