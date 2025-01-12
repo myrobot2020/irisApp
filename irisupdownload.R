8e4b166c564dbcc574b666231fa613f1ed276899
Nik <33054349+myrobot2020@users.noreply.github.com>
2025-01-12 23:18:22 +0530

    Update irisupdownload.R

--- irisupdownload.R
+++ irisupdownload.R
@@ -1,26 +1,17 @@
 library(shiny)
 
-library(shiny)
-
 ui <- fluidPage(
   fileInput("a","a"),
   tableOutput("b"),
   downloadButton("c")
 )
-
 server <- function(input, output, session) {
-  
-  
   df<-reactive({
     read.csv(input$a$datapath)
   })
-  
   output$b<-renderTable({
     df()
-    
   })
-  
-  
   output$c <- downloadHandler(
     filename = function() {
       paste0(input$a, ".csv")
@@ -28,9 +19,6 @@ server <- function(input, output, session) {
     content = function(file) {
       write.csv(df(), file)
     }
-  )
-  
-  
+  ) 
 }
-
 shinyApp(ui, server)
